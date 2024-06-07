dd0df194bc143fef5333fdd4a9df45a8896fbd70
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-07 23:40:43 +0100

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
  * binary_tree_node - Entry point
- * @parent - address of parent node
- * @value - data to be stored in created node
+ * @parent: address of parent node
+ * @value: data to be stored in created node
  *
  * Return: pointer to node on Success, NULL on failure
  */
@@ -17,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp;
 
-        temp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	temp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (!temp)
 		return (NULL);
 
