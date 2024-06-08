0ec939d955b756c0e97129515298f3cf827bd4a9
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-08 11:34:45 +0100

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Entry point
+ *
+ * @tree: pointer to root node
+ * Return: Void
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+		tree = NULL;
+}
