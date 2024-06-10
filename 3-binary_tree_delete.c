48040b090d60ead3d7f67462ae896343c395fd1d
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-10 12:34:37 +0100

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
  * binary_tree_delete - Entry point
- *
+ * 
  * @tree: pointer to root node
+ * 
  * Return: Void
 */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		if (tree->parent)
-			tree->parent = NULL;
-		if (tree->left)
-			tree->parent = NULL;
-		if (tree->right)
-			tree->parent = NULL;
-	}
+    if (tree != NULL)
+        return;
+
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+
+    free(tree);
 }
